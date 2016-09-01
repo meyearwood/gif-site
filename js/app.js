5f1f3ea918d2b7bf899de935099c4b305314141b
Sarah Riehl <sls.riehl@gmail.com>
2016-09-01 16:19:25 -0500

    col height set

--- js/app.js
+++ js/app.js
@@ -8,17 +8,20 @@ var j;
 var hasRating;
 var displaySubject;
 var showButtons;
+var gifThumbHeight;
+var gifThumbHeightNum;
+var imgHeightArr = [];
 
 var shows = [
 	'Murder She Wrote',
 	'Star Trek Voyager',
-	'Star Trek Deep Space Nine',
+	'BeastMaster',
 	'Buffy the Vampire Slayer',
 	'Battlestar Galactica',
 	'Bones',
 	'Quantum Leap',
-	'Law and Order',
-	'Mad Men',
+	'CSI',
+	'Hercules',
 	'Breaking Bad',
 	'The Wire',
 	'Xena Warrior Princess'
@@ -33,28 +36,34 @@ function displayButtons() {
 }
 
 function ajaxQuery(event) {
-	$('.row-gifs').empty();
+	$('.row-gif').empty();
 	subject = $(this).attr('data-subject');
 	queryUrl = ('https://api.giphy.com/v1/gifs/search?q=' + subject + limit + apiKey);
 	console.log(queryUrl);
+
 	$.ajax({url: queryUrl, method: 'GET'})
 	.done(function(response) {
 		console.log(response);
+		$('.col-gif').css({'height': ''});
+		imgHeightArr = [];
+
 		for(i = 0; i < response.data.length; i++) {
 
-			console.log(response.data[i].images.fixed_height.url);
-			respCol = $('<div class="col col-xs-3 col-gif ' + i + '"><img class="gif-thumb" alt="" src="' + response.data[i].images.fixed_height.url + '" /><br /></div>');
+			console.log(response.data[i].images.original.url);
+			respCol = $('<div class="col col-xs-3 col-gif col-' + [i] + '"><img class="gif-thumb img-' + [i] + '" alt="" src="' + response.data[i].images.original.url + '" /><br /></div>');
 			// displaySubject = 
-			// respCol.append('' + subject + '');
-
+			imgHeightArr.push(response.data[i].images.original.height/response.data[i].images.original.width);
+			$('.gif-thumb').on('load', imgHeight);
 			hasRating = response.data[i].rating.toUpperCase();
 			if(response.data[i].rating === '') {
 				respCol.append('<span>rating: NR</span>');
 			} else {
 				respCol.append('<span>rating: ' + hasRating + '</span>');
 			}
-			$('.row-gifs').append(respCol);
+			$('.row-gif').append(respCol);
 		}
+		// respCol.append('' + subject + '');
+			
 
 
 
@@ -62,10 +71,21 @@ function ajaxQuery(event) {
 
 	});
 }
+function imgHeight() {
+		imgHeightArr.sort(function(a, b) {
+			return b - a;
+		});
+		newImgHeight = imgHeightArr[0] * 255
+		$('.col-gif').css({'height': 'calc(' + newImgHeight + 'px + 30px + 1.5em)'});
+}
+
+$(document).ready(function() {
 
 	displayButtons();
 
 	$('.btn-gif').click(ajaxQuery);
+
+});
 
 // page loads with header and start button
 // on click start button, load first question, start timer set interval
